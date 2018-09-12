f28dcfeef5df90107c5c88c6f9d71ed3951bd206
Hancoin - Origin of the EMNetwork - Germany <36687328+hancoin@users.noreply.github.com>
2018-09-12 03:12:19 -0700

    Update

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ################################################
-###       only-update 3DCoin Masternode             ###
+###       only-update ElasticMasternode             ###
 ################################################
 printf "\033[0;37m########################################\033[0m\n" 
-printf '\033[00;32monly-update 3DCoin Masternode\033[0m\n'
+printf '\033[00;32monly-update ElasticMasternode\033[0m\n'
 printf "\033[0;37m########################################\033[0m\n" 
 # update single masternode 
 3dcoin-cli stop
 echo "Please wait......."
 sleep 10
-rm /usr/local/bin/3dcoind
-dir=$(find / -type d -name '3dcoin' -print) 
+rm /usr/local/bin/elasticmasternoded
+dir=$(find / -type d -name 'elasticmasternode' -print) 
 cd $dir
 git checkout master
 git pull
 make install || { ./autogen.sh && ./configure --disable-tests --disable-gui-tests --without-gui && make install;  }
-3dcoind
+elasticmasternoded
